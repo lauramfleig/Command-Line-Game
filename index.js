10b8b2575cd198d8c90decec426be482e51952cc
Laura Fleig <laurafleig@Lauras-MacBook-Pro.local>
2018-02-02 13:30:01 -0800

    stuck

--- index.js
+++ index.js
@@ -7,44 +7,49 @@ function chooseWord() {
     var wordArray = ['English', 'Social Studies', 'Algebra', 'Social Studies']
     var chosenWord = wordArray[Math.floor(Math.random() * wordArray.length)];
     return chosenWord
-    //---------------log the letters in chosenWord
-    var res = chosenWord.split('');
-    console.log(res)
    
 }
 
 
+function startGame() {
 
-// function checkIfCorrect() {
-//     if (guessedLetter === true) {
-//         console.log('correct')
-//     } else { console.log('wtf are you doing') }
-// }
+    console.log('It\'s Time to Play Hangman!!')
+
+    var chosenWord = new word.Word(chooseWord())
+
+    inquirer.prompt([{
+        name: "category",
+        message: "The category is: School Subjects"
+    }, {
+        name: "enterLetter",
+        message: "Enter a Letter to Begin!"
+
+    }]).then(function (answers) {
+        var guessedLetter = answers.enterLetter
+        chosenWord.guessLetter(guessedLetter)
+        console.log(chosenWord.wordToString())
+
+        if (!!!!!!!!!!!)
 
 
-// function startGame() {
+        
+    });
 
-//     console.log('It\'s Time to Play Hangman!!')
+}
 
-//     var chosenWord = new word.Word(chooseWord())
-//     console.log(chosenWord)
+startGame()
 
-//     inquirer.prompt([{
-//         name: "category",
-//         message: "The category is: School Subjects"
-//     }, {
-//         name: "enterLetter",
-//         message: "Enter a Letter to Begin!"
+//TASKS
 
-//     }]).then(function (answers) {
-//         var guessedLetter = answers.enterLetter
-//         chosenWord.guessLetter(guessedLetter)
-//         console.log(chosenWord.wordToString())
-//         if (guessedLetter === ) {
-//             console.log('correct')
-//         } else { console.log('wtf are you doing') }
-//     });
-
-// }
-
-chooseWord()
+//check if the guessed letter is equal to a letter in the chosen word
+    //need to find a way to log all of the letters in the chosen word 
+//if statements
+    //if the guessed letter === letter in chosen word
+        //remaining guesses stays the same
+        //another guess letter prompt is activated
+    //if the guessed letter != letter in chosen word
+        //remaining guesses goes down by one
+            //user is informed
+        //another guess letter prompt is activated
+//if guesses left === 0 
+    //game over
